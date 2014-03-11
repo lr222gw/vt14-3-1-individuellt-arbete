4ee0f8ed869fe2c6e21557e895707f895b747e70
lr222gw <lr222gw@student.lnu.se>
2014-03-11 16:59:36 +0100

    Fixat master page W/ css + innehåll

--- vt14-2-2-aventyrliga-kontakter/Spellistaren/Spellistaren/pages/Shared/Site.Master.designer.cs
+++ vt14-2-2-aventyrliga-kontakter/Spellistaren/Spellistaren/pages/Shared/Site.Master.designer.cs
@@ -22,13 +22,13 @@ namespace Spellistaren.pages.Shared {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder PHTitle;
         
         /// <summary>
-        /// form1 control.
+        /// mainForm control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm mainForm;
         
         /// <summary>
         /// PHContent control.
@@ -38,6 +38,15 @@ namespace Spellistaren.pages.Shared {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder PHContent;
+        
+        /// <summary>
+        /// OutsideContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder OutsideContent;
         
         /// <summary>
         /// PHScript control.
