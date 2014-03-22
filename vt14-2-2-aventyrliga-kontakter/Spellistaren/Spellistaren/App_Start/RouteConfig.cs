64c15ac076cc4f2dfed886411fb05f67230f02b4
lr222gw <lr222gw@student.lnu.se>
2014-03-22 16:40:03 +0100

    Uppdaterade RouteConfig

--- vt14-2-2-aventyrliga-kontakter/Spellistaren/Spellistaren/App_Start/RouteConfig.cs
+++ vt14-2-2-aventyrliga-kontakter/Spellistaren/Spellistaren/App_Start/RouteConfig.cs
@@ -10,6 +10,9 @@ namespace Spellistaren.App_Start
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //EDIT: 2014-03-22 Felet var att jag inte hade raden under... 
+            routes.MapPageRoute("Default2", "", "~/pages/Default.aspx");
+
             routes.MapPageRoute("Default", "Listor", "~/pages/Default.aspx");
             routes.MapPageRoute("AddOrEdit", "Listor/Edit", "~/pages/AddOrEdit.aspx");
         }
